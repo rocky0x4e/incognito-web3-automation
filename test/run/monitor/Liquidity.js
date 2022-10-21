09476d4cba871e69977512706eb7c7d1118a3a19
anhho <anhho0294@gmail.com>
2022-10-21 13:12:37 +0700

    Apply coinService lib for testcase Liquidity

--- test/run/monitor/Liquidity.js
+++ test/run/monitor/Liquidity.js
@@ -1,32 +1,28 @@
-const config = require('../../../constant/config');
-const cliCommonFunction = require('../../../constant/cliCommonFunction');
-const sdkCommonFunction = require('../../../constant/sdkCommonFunction');
-const chainCommonFunction = require('../../../constant/chainCommonFunction');
-const commonFunction = require('../../../constant/commonFunction');
-const coinServiceApi = require('../../../models/coinServiceApi');
 const validateSchemaCommand = require("../../../schemas/validateSchemaCommand");
 const coinServiceApi_schemas = require("../../../schemas/coinServiceApi_schemas");
-const addingContent = require('../../../testbase/addingContent');
-
-//Require the dev-dependencies
 let chai = require('chai');
+const { CoinServiceApi } = require('../../../lib/Incognito/CoinService/CoinServiceApi');
+
+
 
 describe('[Class] Liquidity', () => {
 
-    let nftID = '5756fc0f1661b2b66bc670683ae0f1ef08441c62d91bd22720fa30093ceacaae'
-    let pool_PRV_USDT = '0000000000000000000000000000000000000000000000000000000000000004-076a4423fa20922526bd50b0d7b0dc1c593ce16e15ba141ede5fb5a28aa3f229-33a8ceae6db677d9860a6731de1a01de7e1ca7930404d7ec9ef5028f226f1633'
-    let pool_PRV_ETH = '0000000000000000000000000000000000000000000000000000000000000004-3ee31eba6376fc16cadb52c8765f20b6ebff92c0b1c5ab5fc78c8c25703bb19e-407b251bb4a262391cad3fda612f9b0fd5c282ed0624815450a0cfa53410c6ec'
+    const NFT_ID = '5756fc0f1661b2b66bc670683ae0f1ef08441c62d91bd22720fa30093ceacaae'
+    const pool_PRV_USDT = '0000000000000000000000000000000000000000000000000000000000000004-076a4423fa20922526bd50b0d7b0dc1c593ce16e15ba141ede5fb5a28aa3f229-33a8ceae6db677d9860a6731de1a01de7e1ca7930404d7ec9ef5028f226f1633'
+    const pool_PRV_ETH = '0000000000000000000000000000000000000000000000000000000000000004-3ee31eba6376fc16cadb52c8765f20b6ebff92c0b1c5ab5fc78c8c25703bb19e-407b251bb4a262391cad3fda612f9b0fd5c282ed0624815450a0cfa53410c6ec'
 
+    let coinServiceApi = new CoinServiceApi()
 
     //Testcase
     describe('TC001_PoolShare', async() => {
         it('TC001_CallAPI', async() => {
 
-            let response = await coinServiceApi.poolShare({ nftID })
 
-            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getPoolShareSchemas, response);
+            let response = await coinServiceApi.poolShare(NFT_ID)
 
-            chai.expect(response.Result).to.have.lengthOf.above(1);
+            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getPoolShareSchemas, response.data);
+
+            chai.expect(response.data.Result).to.have.lengthOf.above(1);
 
         })
     })
@@ -41,9 +37,9 @@ describe('[Class] Liquidity', () => {
 
             let response = await coinServiceApi.poolDetail({ poolIDs })
 
-            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getPoolDetailSchemas, response);
+            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getPoolDetailSchemas, response.data);
 
-            for (const pool of response.Result) {
+            for (const pool of response.data.Result) {
                 chai.expect(pool.Token1Value).to.above(0);
                 chai.expect(pool.Token2Value).to.above(0);
                 chai.expect(pool.Virtual1Value).to.above(0);
@@ -61,33 +57,33 @@ describe('[Class] Liquidity', () => {
     describe('TC003_ContributeHistory', async() => {
         it('TC001_CallAPI', async() => {
 
-            let response = await coinServiceApi.contributeHistory({ nftID })
+            let response = await coinServiceApi.contributeHistory({ nftID: NFT_ID })
 
-            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getContributeHistorySchemas, response);
+            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getContributeHistorySchemas, response.data);
 
-            chai.expect(response.Result).to.have.lengthOf.above(1);
+            chai.expect(response.data.Result).to.have.lengthOf.above(1);
         })
     })
 
     describe('TC004_WithdrawHistory', async() => {
         it('TC001_CallAPI', async() => {
 
-            let response = await coinServiceApi.withdrawHistory({ nftID })
+            let response = await coinServiceApi.withdrawHistory({ nftID: NFT_ID })
 
-            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getWithdrawHistorySchemas, response);
+            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getWithdrawHistorySchemas, response.data);
 
-            chai.expect(response.Result).to.have.lengthOf.above(0);
+            chai.expect(response.data.Result).to.have.lengthOf.above(0);
         })
     })
 
     describe('TC005_WithdrawFeeHistory', async() => {
         it('TC001_CallAPI', async() => {
 
-            let response = await coinServiceApi.withdrawFeeHistory({ nftID })
+            let response = await coinServiceApi.withdrawFeeHistory({ nftID: NFT_ID })
 
-            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getWithdrawFeeHistorySchemas, response);
+            await validateSchemaCommand.validateSchema(coinServiceApi_schemas.getWithdrawFeeHistorySchemas, response.data);
 
-            chai.expect(response.Result).to.have.lengthOf.above(0);
+            chai.expect(response.data.Result).to.have.lengthOf.above(0);
         })
     })
 
