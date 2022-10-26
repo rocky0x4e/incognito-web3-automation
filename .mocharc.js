6b2782876bec447e445c754064ec2e6a4ac8b264
anhho <anhho0294@gmail.com>
2022-10-26 14:38:06 +0700

    Update testcase webBaseV1/BE_EvmShield.js

--- .mocharc.js
+++ .mocharc.js
@@ -6,13 +6,15 @@ module.exports = {
     timeout: 20000,
     file: [
         './TestCases/TestBase.js',
-        './Testcases/monitor/backend/Login.js',
-        './Testcases/monitor/backend/Papp.js',
-        './Testcases/monitor/coinService/Balance.js',
-        './Testcases/monitor/coinService/Liquidity.js',
-        './Testcases/monitor/coinService/Market.js',
-        './Testcases/monitor/coinService/Pdex.js',
-        './Testcases/webBaseV1/BE_EstimateSwap.js',
+
+        // './Testcases/monitor/backend/Login.js',
+        // './Testcases/monitor/backend/Papp.js',
+        // './Testcases/monitor/coinService/Balance.js',
+        // './Testcases/monitor/coinService/Liquidity.js',
+        // './Testcases/monitor/coinService/Market.js',
+        // './Testcases/monitor/coinService/Pdex.js',
+        // './Testcases/webBaseV1/BE_EstimateSwap.js',
+        './Testcases/webBaseV1/BE_EvmShield.js',
     ],
     reporter: 'node_modules/mochawesome',
     'reporter-option': [
