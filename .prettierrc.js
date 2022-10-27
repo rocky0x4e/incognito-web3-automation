0d4a38fa6f0404c47ca2f354277c660ee2178642
anhho <anhho0294@gmail.com>
2022-10-27 14:46:51 +0700

    Add formater .prettierrc.js

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  semi: true,
+  tabWidth: 2,
+};
---
+++ TestCases/Demo.js
@@ -0,0 +1,10 @@
+const {IncNode} = require("../lib/Incognito/IncNode");
+
+const run = async() => {
+
+    let node = new IncNode()
+    console.log('hoanh node', node);
+
+}
+
+run()
