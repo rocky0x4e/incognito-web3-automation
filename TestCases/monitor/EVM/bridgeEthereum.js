b1c89953b11710e5625fe2cbcaa1dff72c865a36
tuan <tuan@incognito.org>
2022-10-28 16:49:51 +0700

    add tc shielding ethereum

---
+++ TestCases/monitor/EVM/bridgeEthereum.js
@@ -0,0 +1,174 @@
+const Constants =require('../../../lib/Incognito/Constants');
+const validateSchemaCommand = require("../../../schemas/validateSchemaCommand");
+const backendApischemas = require("../../../schemas/backendApi_schemas");
+const { BackendApi } = require('../../../lib/Incognito/BackendApi')
+const { IncAccount } = require('../../../lib/Incognito/Account/Account');
+const { IncNode } = require('../../../lib/Incognito/IncNode');
+const { CoinServiceApi } = require('../../../lib/Incognito/CoinService/CoinServiceApi');
+const { ENV } = require('../../../global');
+const addingContent = require('../../../testbase/addingContent');
+const { wait } = require('../../../lib/Utils/Timer');
+let fs = require('fs');
+let Tx = require('ethereumjs-tx').Transaction;
+let Common = require('ethereumjs-common').default;
+// const web3CommonFuntion = require('../../../constant/web3CommonFuntion');
+
+let chai = require('chai');
+let Web3 = require('web3');
+
+
+describe(`[Ethereum brigde]`, async() =>{
+    const extPrivateKey = '0xd455f2de1aa18787ea5820afce2ae95b7405d11b9eb19d340f6f2d821047d437'
+    const privateKey =`112t8rnX3VTd3MTWMpfbYP8HGY4ToAaLjrmUYzfjJBrAcb8iPLkNqvVDXWrLNiFV5yb2NBpR3FDZj3VW8GcLUwRdQ61hPMWP3YrREZAZ1UbH`
+    const tokenID = Constants.TOKEN.ETH
+    const tokenUnifiedID = Constants.TOKEN.UnifiedETH
+
+    let node = await new IncNode()
+    let account = await new IncAccount(privateKey).attachTo(node)
+    let coinServiceApi = await new CoinServiceApi(ENV.CoinService)
+    let backendApi =await new BackendApi(ENV.Backend)
+  
+    let web3 = await new Web3(new Web3.providers.HttpProvider(ENV.EthereumFullnode[0].url))
+    let extAccount = await web3.eth.accounts.privateKeyToAccount(extPrivateKey)
+
+    let SignPublicKeyEncode ='f78fcecf2b0e2b3267d5a1845c314b76f3787f86981c7abcc5b04abc49ae434a';
+   
+    const accountInfoBefore = {
+        incTokenBal : 0,
+        extTokenBal : 0
+    }
+    const accountInfoAfter = {
+        incTokenBal : 0,
+        extTokenBal : 0
+    }
+
+    const shieldInfo = {
+        shieldPrvFee : 0,
+        shieldTokenFee : 0,
+        tmpWalletAddress : null
+    }
+    
+
+    describe('SHIELDING', async() => {
+        it('Initdata', async() => {
+            accountInfoBefore.incTokenBal = await account.useCli.getBalance(tokenID)
+            await console.log('accountInfoBefore: ',accountInfoBefore.incTokenBal )
+        })
+    })
+
+    describe(`TC001_shielding_ETH`, async()=>{
+        it('STEP_01_Call_API_Gen_Shield_Address', async() => {
+            let res = await backendApi.ethGenerate(1, account.paymentK, tokenID,SignPublicKeyEncode)
+            await validateSchemaCommand.validateSchema(backendApischemas.generateShieldAddressSchemas,res.data)
+            shieldInfo.tmpWalletAddress = res.data.Result.Address
+            shieldInfo.shieldTokenFee = res.data.Result.EstimateFee
+            console.log('tmp wallet address : ' + shieldInfo.tmpWalletAddress)
+            console.log('estimate shielding fee : ' + shieldInfo.shieldTokenFee)
+        })
+    })
+
+    describe.skip(`[WEB3] Deposit token`, async() => {
+        it(`Get balance before deposit`, async() =>{
+            accountInfoBefore.extTokenBal = await web3.eth.getBalance(extAccount.address)
+            await console.log('accountInfoBefore.extTokenBal: ',accountInfoBefore.extTokenBal )
+            tmpWalletBal = await web3.eth.getBalance(extAccount.address)
+            await console.log('tmpWalletBal: ',tmpWalletBal )
+        });
+        it(`Deposit token`, async() =>{
+            await console.log('sender %s -- receiver %s ', extAccount.address,shieldInfo.tmpWalletAddress)
+            await sendNativeToken(extAccount.address,shieldInfo.tmpWalletAddress,extPrivateKey,(0.01+shieldInfo.shieldTokenFee)*1e18,ENV.EthereumFullnode[0].url)
+            await wait(10)
+            accountInfoAfter.extTokenBal = await web3.eth.getBalance(extAccount.address)
+            await console.log('accountInfoBefore.extTokenBal: ',accountInfoBefore.extTokenBal )
+            tmpWalletBal = await web3.eth.getBalance(extAccount.address)
+            await console.log('tmpWalletBal new: ',tmpWalletBal )   
+        })
+    })
+    describe(`TC002_Verify_record_shield_backend`, async() => {
+        let shieldID 
+        it('Waiting create shielding record', async() => {
+            let resBefore = await backendApi.historyByTokenAccount(account.paymentK, tokenUnifiedID,SignPublicKeyEncode)
+            console.log('aaaaa : ',resBefore.data)
+            await validateSchemaCommand.validateSchema(backendApischemas.historyTokenAccountSchemas,resBefore.data)
+            shieldID = await pickNewShield(account.paymentK, tokenUnifiedID,SignPublicKeyEncode)
+            console.log('newShieldID : ', shieldID)
+        })
+        it('Verify shielding detail', async() => {
+            let resDetail = await backendApi.historyDetail(shieldID,1,SignPublicKeyEncode,2)
+            console.log(resDetail.data)
+            await validateSchemaCommand.validateSchema(backendApischemas.historyTokenAccountDetailSchemas,resDetail.data)
+
+            while(resDetail.data.Result.Status != 12){
+                let tmp = await backendApi.historyDetail(shieldID,1,SignPublicKeyEncode,2)
+                console.log('Shield status = ' +  tmp.data.Result.Status + ' ---- ' + tmp.data.Result.StatusMessage + ' --- '+ tmp.data.Result.StatusDetail)
+                resDetail.data.Result.Status = tmp.data.Result.Status
+                if(resDetail.data.Result.Status === 12 ){
+                    console.log('Shielding successfull')
+                    break
+                }
+                await wait(120)
+            }
+        })
+    })
+    describe(`TC003_Verify_balance_in_Incognito`, async() => {
+        it('Verify balance affter shield', async() => {
+            await wait(20)
+            accountInfoAfter.incTokenBal = await account.useCli.getBalance(tokenID)
+            console.log('accountInfoAfter: ',accountInfoAfter.incTokenBal )
+            chai.assert.notEqual(accountInfoBefore.incTokenBal,accountInfoAfter.incTokenBal, 'mint token unsuceessfull')
+        })
+        // it('Verify balance', async() => {
+            
+        // })
+    })
+})
+
+
+async function pickNewShield(WalletAddress,
+    PrivacyTokenAddress,
+    SignPublicKeyEncode ='f78fcecf2b0e2b3267d5a1845c314b76f3787f86981c7abcc5b04abc49ae434a'
+     ) {
+    let backendApi =await new BackendApi(ENV.Backend)
+    let id = 0
+    const resBefore = await backendApi.historyByTokenAccount(WalletAddress, PrivacyTokenAddress,SignPublicKeyEncode)
+    console.log(resBefore.data)
+    await validateSchemaCommand.validateSchema(backendApischemas.historyTokenAccountSchemas,resBefore.data)
+  
+    for (let i = 0 ; i <5; i++){
+        let resAfter = await backendApi.historyByTokenAccount(WalletAddress, PrivacyTokenAddress,SignPublicKeyEncode)
+        console.log('resAfter.data.Result.length = ' + resAfter.data.Result.length + '----' + 'resBefore.data.Result.length = ' + resBefore.data.Result.length   )
+        console.log ('Waiting backend listen tx shield .... ')
+        if(resAfter.data.Result.length > resBefore.data.Result.length) {
+            const idList = []
+            for (const iterator of resAfter.data.Result) {
+                idList.push(iterator.ID)
+            }
+            id = idList.sort(function(a, b){return b - a});
+            return id[0]
+        }
+        await wait(120) 
+    }
+    return id
+}
+
+
+async function sendNativeToken(fromAddress, toAddress, pk, amountToSend, networkNode){
+    let  web3 = await new Web3(new Web3.providers.HttpProvider(networkNode))
+    let privateKey =await Buffer.from(pk.slice(2), 'hex')
+    let count = await web3.eth.getTransactionCount(fromAddress)
+
+    let rawTransaction = {
+        "gasPrice": web3.utils.toHex(web3.utils.toWei('90', 'gwei')),
+        "gasLimit": web3.utils.toHex(220000),
+        "to": toAddress,
+        "value": web3.utils.toHex(amountToSend),
+        "nonce": web3.utils.toHex(count)
+    }
+
+    let transaction = new Tx(rawTransaction, { chain: 'goerli'})
+    transaction.sign(privateKey)
+
+    let result = await web3.eth.sendSignedTransaction('0x' + transaction.serialize().toString('hex'))
+    console.log(result)
+    return result
+}
--- TestCases/monitor/EVM/monitorFullnodeEVM.js
+++ TestCases/monitor/EVM/monitorFullnodeEVM.js
@@ -1,18 +1,18 @@
 const { ENV } = require('../../../global');
 let Web3 = require('web3');
 let chai = require('chai');
-const web3CommonFuntion = require('../../../constant/web3CommonFuntion');
+// const web3CommonFuntion = require('../../../constant/web3CommonFuntion');
 const { wait } = require('../../../lib/Utils/Timer');
 
 
-describe(`[EVM] Fullnode`, () => {
+describe.skip(`[EVM] Fullnode`, () => {
     describe(`TC001_Ethereum_Fullnode`, async () => {    
         it(`Call Fullnode`, async () => {
             const host = ENV.EthereumFullnode
             console.log(host)
-            const blockNumList1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNumList1 = await getBlockNumList(host)
             await wait(20)
-            const blockNumList2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNumList2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNumList1, blockNumList2, `seem fullnode die`)
             
         }).timedOut(200000)
@@ -21,9 +21,9 @@ describe(`[EVM] Fullnode`, () => {
         it(`Call Fullnode`, async () => {
             const host = ENV.BSCFullnode
             console.log(host)
-            const blockNum1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum1 = await getBlockNumList(host)
             await wait(20)
-            const blockNum2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNum1, blockNum2, `seem fullnode die`)
         }).timedOut(200000)
     })
@@ -31,9 +31,9 @@ describe(`[EVM] Fullnode`, () => {
         it(`Call Fullnode`, async () => {
             const host = ENV.PLGFullnode
             console.log(host)
-            const blockNum1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum1 = await getBlockNumList(host)
             await wait(20)
-            const blockNum2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNum1, blockNum2, `seem fullnode die`)
         }).timedOut(200000)
     })
@@ -41,9 +41,9 @@ describe(`[EVM] Fullnode`, () => {
         it(`Call Fullnode`, async () => {
             const host = ENV.FTMFullnode
             console.log(host)
-            const blockNum1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum1 = await getBlockNumList(host)
             await wait(20)
-            const blockNum2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNum1, blockNum2, `seem fullnode die`)
         }).timedOut(200000)
     })
@@ -51,9 +51,9 @@ describe(`[EVM] Fullnode`, () => {
         it(`Call Fullnode`, async () => {
             const host = ENV.AuroraFullnode
             console.log(host)
-            const blockNum1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum1 = await getBlockNumList(host)
             await wait(20)
-            const blockNum2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNum1, blockNum2, `seem fullnode die`)
         }).timedOut(200000)
     })
@@ -61,10 +61,27 @@ describe(`[EVM] Fullnode`, () => {
         it(`Call Fullnode`, async () => {
             const host = ENV.AvaxFullnode
             console.log(host)
-            const blockNum1 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum1 = await getBlockNumList(host)
             await wait(20)
-            const blockNum2 = await web3CommonFuntion.getBlockNumList(host)
+            const blockNum2 = await getBlockNumList(host)
             chai.assert.notEqual(blockNum1, blockNum2, `seem fullnode die`)
         }).timedOut(200000)
     })
 })
+
+
+
+async function getBlockNumList(hostlist) {
+    const blockNumList =[]
+    for (let i of hostlist){
+        let web3 = await new Web3(new Web3.providers.HttpProvider(i.url));
+        await web3.eth.getBlockNumber().then((blockNum) => {
+            // console.log('block Num: %s %d', i ,blockNum)
+            blockNumList.push(blockNum)
+        }).catch(err =>{
+            blockNumList.push(0)
+            console.log('err :', err)
+        })
+    }
+    return blockNumList 
+}
