0c0b23fd18aecdfd35e3012b9ffd39321454f1af
Rocky <thach@incognito.org>
2022-11-14 18:10:17 +0700

    refactor: load testdata from file

--- TestCases/monitor/backend/Login.js
+++ TestCases/monitor/backend/Login.js
@@ -1,6 +1,5 @@
 const validateSchemaCommand = require("../../../schemas/validateSchemaCommand");
 const backendApi_schemas = require("../../../schemas/backendApi_schemas");
-const { ENV } = require('../../../global');
 const { BackendApi } = require('../../../lib/Incognito/BackendApi');
 
 
@@ -8,24 +7,24 @@ let backendApi = new BackendApi()
 
 describe('[Class] Login', () => {
 
-    describe('TC001_Profile', async() => {
-        it('CallAPI', async() => {
+    describe('TC001_Profile', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.authProfile()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.authenProfileSchemas, response.data)
         })
     })
 
-    describe('TC002_DisableFunctionConfig', async() => {
-        it('CallAPI', async() => {
+    describe('TC002_DisableFunctionConfig', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.disableFunctionConfig()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.disableFunctionConfigSchemas, response.data)
         })
     })
 
-    describe('TC003_LastVersion', async() => {
-        it('CallAPI', async() => {
+    describe('TC003_LastVersion', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.lastVersion()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.lastVersionSchemas, response.data)
