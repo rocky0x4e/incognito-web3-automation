c6561df226e8d9f21629d4f7f9fdee650bdb86af
anhho <anhho0294@gmail.com>
2022-11-14 12:02:55 +0700

    Init endToEnd/papp/Papp.js

---
+++ TestCases/endToEnd/papp/Papp.js
@@ -0,0 +1,97 @@
+const { TOKEN, POOL } = require('../../../lib/Incognito/Constants')
+const listAccount = require("../../../constant/listAccount.json");
+const { IncNode } = require("../../../lib/Incognito/IncNode");
+const { IncAccount } = require("../../../lib/Incognito/Account/Account");
+const { IncRpc } = require("../../../lib/Incognito/RPC/Rpc");
+const GenAction = require("../../../lib/Utils/GenAction");
+let chai = require("chai");
+const { getLogger } = require("../../../lib/Utils/LoggingManager");
+const { CoinServiceApi } = require('../../../lib/Incognito/CoinServiceApi');
+const { WebServiceApi } = require('../../../lib/Incognito/WebServiceApi');
+const logger = getLogger("Pdex")
+
+let incRpc = new IncRpc();
+let incNode = new IncNode()
+let coinServiceApi = new CoinServiceApi()
+let webServiceApi = new WebServiceApi()
+let sender = new IncAccount(listAccount[3], incNode)
+
+describe("[Class] Papp", () => {
+    describe.skip("TC001_AddExistLiquidity", async() => {
+        let ammountSell = 0
+        let estimateFeeObject
+        let tx
+        let sellTokenID
+        let buyTokenID
+        let networks = 'bsc'
+
+        it("STEP_InitData", async() => {
+            await sender.initSdkInstance();
+
+            //getBalance
+            let balanceAll = await sender.useCli.getBalanceAll()
+
+            sellTokenID = TOKEN.USDT_UT
+            buyTokenID = TOKEN.DAI_UT
+            sender.balancePRVBefore = balanceAll[TOKEN.PRV]
+            sender.balanceUSDTBefore = balanceAll[sellTokenID]
+            sender.balanceDAIBefore = balanceAll[buyTokenID]
+            console.log("hoanh sender.balanceBefore", balancePRVBefore, sender.balanceUSDTBefore, sender.balanceDAIBefore);
+
+            ammountSell = await GenAction.randomNumber(100000)
+        }).timeout(60000);
+
+        it("STEP_EstimateTrade", async() => {
+            let response = (await webServiceApi.estimateSwapFee({
+                amount: ammountSell,
+                fromToken: sellTokenID,
+                network: "inc",
+                slippage: "0.5",
+                toToken: buyTokenID
+            }))
+
+            estimateFeeObject = response.data.Result.Networks[networks][0]
+        }).timeout(120000);
+
+
+        it("STEP_CreateTx", async() => {
+            let tx = sender.useSdk.swapPapp({
+                sellTokenID: sellTokenID,
+                senderFeeAddressShardID, // ???
+
+                feeReceiverAddress, // ???
+                feeAmount: estimateFeeObject.Fee.amount,
+                feeTokenID: estimateFeeObject.Fee.tokenid,
+
+                // data metadata
+                sellAmount: ammountSell,
+                callContract, // proxy route
+                callData,
+                exchangeNetworkID, // networkID exchange, exp: ETH = 1
+                sellChildTokenID,
+                buyContractID,
+                // remoteAddress, case reDeposit = 0x0000000000000000000000000000000000000000
+                // send out EVN use user address
+                remoteAddress,
+                buyTokenID,
+                sellAmountText,
+                buyAmountText
+            })
+        }).timeout(60000);
+
+
+        it("STEP_VerifyOutchain", async() => {
+            //TODO
+        }).timeout(60000);
+
+        it("STEP_VerifySwapTxStatus", async() => {
+            //TODO
+        }).timeout(60000);
+
+        it("STEP_VerifyBalance", async() => {
+            //TODO
+        }).timeout(60000);
+    });
+
+
+});
--- TestCases/monitor/backend/Papp.js
+++ TestCases/monitor/backend/Papp.js
@@ -10,84 +10,83 @@ let privateKey = listAccount.main7
 var backendApi = new BackendApi()
 var account = new IncAccount(privateKey)
 
-describe('[CLASS] Papp', async() => {
+describe('[CLASS] Papp', async () => {
 
-    describe('TC001_PancakeTradeToken', async() => {
-        it('CallAPI', async() => {
+    describe('TC001_PancakeTradeToken', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.tradeToken()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.tradeTokenSchemas, response.data)
         })
 
-        it('CallAPI_2', async() => {
+        it('CallAPI_2', async () => {
             let response = await backendApi.uniswapToken()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.uniSwapTokenSchemas, response.data)
         })
     })
 
-    describe('TC002_UniswapToken', async() => {
-        it('CallAPI', async() => {
+    describe('TC002_UniswapToken', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.uniswapToken()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.uniSwapTokenSchemas, response.data)
         })
     })
 
-    describe('TC003_CurveToken', async() => {
-        it('CallAPI', async() => {
+    describe('TC003_CurveToken', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.curveToken()
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.curveTokenSchemas, response.data)
         })
     })
 
-    describe('TC004_TradeHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC004_TradeHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.tradeHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.tradeHistorySchemas, response.data)
         })
     })
 
-    describe('TC005_UniswapHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC005_UniswapHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.uniswapHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.uniswapHistorySchemas, response.data)
         })
     })
 
-    describe('TC006_CurveHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC006_CurveHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.curveHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.curveHistorySchemas, response.data)
         })
     })
 
-    describe('TC007_TradeRewardHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC007_TradeRewardHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.tradeRewardHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.tradeRewardHistorySchemas, response.data)
         })
     })
 
-    describe('TC008_UniswapRewardHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC008_UniswapRewardHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.uniswapRewardHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.uniswapRewardHistorySchemas, response.data)
         })
     })
 
-    describe('TC009_CurveRewardHistory', async() => {
-        it('CallAPI', async() => {
+    describe('TC009_CurveRewardHistory', async () => {
+        it('CallAPI', async () => {
             let response = await backendApi.curveRewardHistory(account.paymentK)
 
             await validateSchemaCommand.validateSchema(backendApi_schemas.curveRewardHistorySchemas, response.data)
         })
     })
-
 })
