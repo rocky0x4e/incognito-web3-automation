747a46baa81718579c38492c00aa2fa606460b99
khanhlh-incognito <khanh@incognito.org>
2022-11-16 16:19:03 +0700

    refactor pnode testscript

--- TestCases/monitor/backend/pNode.js
+++ TestCases/monitor/backend/pNode.js
@@ -1,25 +1,12 @@
-const commonFunction = require("../../../constant/commonFunction");
-const validateSchemaCommand = require("../../../schemas/validateSchemaCommand");
-const coinServiceApi_schemas = require("../../../schemas/coinServiceApi_schemas");
-const addingContent = require("../../../lib/Utils/AddingContent");
 let chai = require("chai");
-const { IncAccount } = require("../../../lib/Incognito/Account/Account");
-const { IncNode } = require("../../../lib/Incognito/IncNode");
 const { CoinServiceApi } = require("../../../lib/Incognito/CoinServiceApi");
 const { BackendApi } = require("../../../lib/Incognito/BackendApi");
 const { ENV } = require("../../../global");
 const { getLogger } = require("../../../lib/Utils/LoggingManager");
-const { TOKEN } = require("../../../lib/Incognito/Constants");
-const logger = getLogger("pNode Testcase");
+const { ACCOUNTS } = require("../../TestBase");
+const logger = getLogger("pNode");
 
-let node = new IncNode(ENV.urlFullNode);
-let sender = new IncAccount("112t8rnbmCZEvEhhfhzZX3HkbqbRHApaJU1veRNMsXg3VWcuQw6P5vLQkeQYKFF8nYHhx4swt4afUDsAAZqFGqLbgTZXZagT84GuF7zoF5rD").attachTo(
-    node
-);
-let receiver = new IncAccount({
-    PaymentAddress:
-        "12suG5oV5KQspoUPseBAnLCmm8vBPQs3je7kbiLuBSyvhAG2dHbo3RP5zRsNoB9Y2m9fA342MyfbpoUJYNcS5zhB5pU89kUiU3YPGDCjh8Eg7Y5HdgAU33XrNQ3q77J5BwThnmcXZvyNekF6EnSr",
-}).attachTo(node);
+let sender = ACCOUNTS.Incognito.get(3);
 let coinServiceApi = new CoinServiceApi();
 let backendApi = new BackendApi(ENV.Backend);
 
@@ -40,13 +27,12 @@ describe("[Class] pNode", () => {
 
     describe("TC007_ pNode", async () => {
         it(" STEP get CURRENT reward and balance", async () => {
+            console.log(`sender privatekey: ${sender.privateK}`);
             await sender.initSdkInstance();
             senderBalancePRVBefore = await sender.useSdk.getBalance();
-            // sender.balancePRVBefore = balanceAll[TOKEN.PRV]
             logger.info(`sender.balancePRVBefore: ${senderBalancePRVBefore}`);
 
             let pNodeReward = await backendApi.getpNodeInfo();
-            // console.log(pNodeReward.Result[0])
             if (pNodeReward.Result[0].Rewards != null) {
                 pNodeRewardAmount = pNodeReward.Result[0].Rewards[0].Amount;
                 logger.info(`pNode Reward Amount = ${pNodeRewardAmount}`);
@@ -78,23 +64,22 @@ describe("[Class] pNode", () => {
         });
 
         it(" STEP get NEW reward and balance", async () => {
-            let pNodeReward = await backendApi.getpNodeInfo();
-            chai.expect(pNodeReward.Result[0].Rewards).to.equal(null);
+            if (execute_test) {
+                let pNodeReward = await backendApi.getpNodeInfo();
+                chai.expect(pNodeReward.Result[0].Rewards).to.equal(null);
 
-            senderBalancePRVAfter = await sender.useCli.waitBalanceChange({
-                from: senderBalancePRVBefore,
-                amountChange: pNodeRewardAmount,
-                checkInterval: 30,
-                timeout: 300,
-            });
-            // sender.balancePRVBefore = balanceAll[TOKEN.PRV]
-            logger.info(`sender.balancePRVAfter: ${senderBalancePRVAfter}`);
-            // if (pNodeReward.Result[0].Rewards == null) {
-            //     pNodeRewardAmount = pNodeReward.Result[0].Rewards[0].Amount
-            //     logger.info(`pNode Reward Amount = ${pNodeRewardAmount}`)}
-            // else {
-            //
-            // }
+                senderBalancePRVAfter = await sender.useCli.waitBalanceChange({
+                    from: senderBalancePRVBefore,
+                    amountChange: pNodeRewardAmount,
+                    checkInterval: 30,
+                    timeout: 300,
+                });
+                chai.expect(senderBalancePRVAfter - senderBalancePRVAfter).to.be.equal(pNodeRewardAmount);
+                logger.info(`sender.balancePRVAfter: ${senderBalancePRVAfter}`);
+            } else {
+                it.skip;
+                logger.info("Checking balance after withdraw reward: SKIPPED");
+            }
         }).timeout(400000);
     });
 });
